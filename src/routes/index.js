7a4cdd31ebc8c7f84e004b7583fc883bc6b0bc8a
dawplus <dawschae@gmail.com>
2022-03-10 16:05:42 +0900

    First Commit

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import { useRoutes } from 'react-router-dom';
+
+// routes
+import MainRoutes from './MainRoutes';
+import AuthenticationRoutes from './AuthenticationRoutes';
+import config from 'config';
+
+export default function ThemeRoutes() {
+    return useRoutes([MainRoutes, AuthenticationRoutes], config.basename);
+}
